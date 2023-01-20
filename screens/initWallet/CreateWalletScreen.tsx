9d21a1d577c056b1b0c526661491e0b6fd106b34
Justin Chen <ch3njus@gmail.com>
2023-01-20 14:26:12 -0500

    feat: seed phrase instructions and list

--- screens/initWallet/CreateWalletScreen.tsx
+++ screens/initWallet/CreateWalletScreen.tsx
@@ -1,17 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
-import Button from '../../components/home/Button';
+import { useState } from 'react';
+
+const renderWord = ({ item }) => (
+  <Text style={styles.item}>{item.word}</Text>
+);
 
 export default function CreateWalletView({ navigation }) {
-    const pressHandler = () => {
-        navigation.navigate('back');
-    }
+    const seedPhrase = [
+      { word: 'word1', id: '1'},
+      { word: 'word2', id: '2'},
+      { word: 'word3', id: '3'},
+      { word: 'word4', id: '4'},
+      { word: 'word5', id: '5'},
+      { word: 'word6', id: '6'},
+      { word: 'word7', id: '7'},
+      { word: 'word8', id: '8'},
+      { word: 'word9', id: '9'},
+      { word: 'word10', id: '10'},
+      { word: 'word11', id: '11'},
+      { word: 'word12', id: '12'}
+    ]
 
     return (
       <View style={ styles.container }>
-        <Text style={{ color: '#fff' }}>Algorand Mobile Wallet</Text>
+        <Text style={styles.header}>Seed Phrase</Text>
+        <Text style={styles.instructions}>Save your seed phrase in a secure location. Never share your seed phrase with anyone.</Text>
         <StatusBar style="auto" />
+        <View>
+          <FlatList
+            keyExtractor={item => item.id}
+            data={seedPhrase}
+            renderItem={renderWord}
+            style={styles.seedPhrase}/>
+        </View>
         <View style={styles.footerContainer}>
         </View>
       </View>
@@ -24,7 +47,26 @@ container: {
     backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
-    // , flex: 1, alignItems: 'center', justifyContent: 'center'
+    paddingTop: 100
+},
+header: {
+  color: '#fff',
+  fontSize: 18
+},
+seedPhrase: {
+  flex: 1
+},
+instructions: {
+  marginTop: 5,
+  color: '#fff',
+  paddingLeft: 30,
+  paddingRight: 30
+},
+item: {
+  flex: 1,
+  color: '#fff',
+  fontSize: 18,
+  paddingTop: 12
 },
 footerContainer: {
     flex: 1 / 3,
