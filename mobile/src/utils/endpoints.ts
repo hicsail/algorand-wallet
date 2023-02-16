9dfbe7985365cb90f65fd6ff53db7ca79ccc1266
Justin Chen <ch3njus@gmail.com>
2023-02-16 12:06:30 -0500

    feat: add endpoints

---
+++ mobile/src/utils/endpoints.ts
@@ -0,0 +1,11 @@
+export const BETANET = {
+    endpoint: 'https://betanet-algorand.api.purestake.io/ps2'
+}
+
+export const TESTNET = {
+    endpoint: 'https://testnet-algorand.api.purestake.io/ps2'
+}
+
+export const MAINNET = {
+    endpoint: 'https://mainnet-algorand.api.purestake.io/ps2'
+}
